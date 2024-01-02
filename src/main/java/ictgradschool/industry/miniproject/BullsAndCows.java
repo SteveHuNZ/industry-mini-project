3d513ae7ef68ef74e37adaa5dd8498b522400b51
stevehu <stevehuinnz@gmail.com>
2024-01-02 23:53:38 +1300

    First three tasks have polished. Focus on the fourth one.

--- src/main/java/ictgradschool/industry/miniproject/BullsAndCows.java
+++ src/main/java/ictgradschool/industry/miniproject/BullsAndCows.java
@@ -5,7 +5,6 @@ public class BullsAndCows {
     public static void start(){
         Random random = new Random();
         int[] sysSecret = new int[4];
-
         Set<Integer> uniqueNum = new HashSet<>();
         // HashSet can only add unique data;
         for(int i = 0; i < sysSecret.length; i++){
@@ -17,39 +16,42 @@ public class BullsAndCows {
             // continue the loop and keep passing random generated number to variable "ran";
             sysSecret[i] = ran;
         }
-
-
-//get user's input number '
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your secret code: ");
-        String str1 = input.nextLine().trim();
+
         int userInputSecret = 0;
         // verifying sanity for input.
-        if(str1.length() ==4){
-            try{
-                userInputSecret = Integer.parseInt(str1);
-            }catch(NumberFormatException e){
-                System.out.println("Invalid input!");
+        while(true){
+            System.out.println("Please enter your secret code: ");
+            String str1 = input.nextLine().trim();
+            if(str1.length() ==4){
+                try{
+                    userInputSecret = Integer.parseInt(str1);
+                    break;
+                }catch(NumberFormatException e){
+                    System.out.println("Invalid input!(4 digit numbers)Try again!");
+                }
+            }else{
+                System.out.println("Invalid length!(4 digit numbers)Try again!");
             }
         }
         int[] userSecret = new int[4];
         getDigit(userSecret,userInputSecret);
-
-
         System.out.println("-".repeat(3));
         int chance = 7;
         while(chance > 0){
-            System.out.print("You guess: ");
-
-            String str2 = input.nextLine().trim();
             int userInputGuess =0;
-            if(str2.length() ==4){
-                try{
-                    userInputGuess = Integer.parseInt(str1);
-                }catch(NumberFormatException e){
-                    System.out.println("Invalid input!");
-
-
+            while(true){
+                System.out.print("You guess(" + chance + " chances remaining): ");
+                String str2 = input.nextLine().trim();
+                if(str2.length() ==4){
+                    try{
+                        userInputGuess = Integer.parseInt(str2);
+                        break;
+                    }catch(NumberFormatException e){
+                        System.out.println("Invalid input!(4 digit numbers)Try again!");
+                    }
+                }else{
+                    System.out.println("Invalid length!(4 digit numbers)Try again!");
                 }
             }
             int[] userGuess = new int[4];
@@ -73,17 +75,12 @@ public class BullsAndCows {
             System.out.println("");
             System.out.println(check(userSecret, sysGuess));
             System.out.println("-".repeat(3));
-
-
-
-
             chance--;
             if (chance == 0) {
-                System.out.println("Game over. Secret code: " + Arrays.toString(sysSecret));
+                System.out.println("Game over. Draw! Secret code: " + Arrays.toString(sysSecret));
             }
         }
     }
-
     public static String check(int[]arr1, int[]arr2){
         int bulls = 0;
         int cows = 0;
@@ -101,23 +98,14 @@ public class BullsAndCows {
         if(bulls == 4) {
             return "Result: 4 bulls and 0 cows\n" + "You win! :)";
         }
-
         return "Result: " + bulls + "bull(s) and " + cows + "cow(s)";
     }
-
-
     public static void getDigit(int[] array, int num){
         array[0] = num / 1000;
         array[1] = num / 100 % 10;
         array[2] = num / 10 % 10;
         array[3] = num % 10;
     }
-
-
-
-
-
-
     public static void main(String[] argus){
         start();
     }
